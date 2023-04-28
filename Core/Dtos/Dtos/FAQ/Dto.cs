88f42afc51dda5d3001176586dfc8b8901da6189
Ahmed Elsheikh <TheAlchimest5@gmail.com>
2023-04-28 17:56:30 +0300

    changes

--- Core/Dtos/Dtos/FAQ/Dto.cs
+++ Core/Dtos/Dtos/FAQ/Dto.cs
@@ -6,9 +6,9 @@ using TemplateFw.Resources;
 
 namespace TemplateFw.Dtos
 {
-    public class FaqDto
-    {
-        public int FaqId { get; set; }
+	public class FaqDto
+	{
+		public int FaqId { get; set; }
 		public string QuestionAr { get; set; }
 		public string QuestionEn { get; set; }
 		public string AnswerAr { get; set; }
@@ -21,11 +21,11 @@ namespace TemplateFw.Dtos
 		public string LastModifiedBy { get; set; }
 		public DateTime? LastModificationDate { get; set; }
 
-    }
+	}
 
-    public class FaqInfoDto
-    {
-        public int FaqId { get; set; }
+	public class FaqInfoDto
+	{
+		public int FaqId { get; set; }
 		public string Question { get; set; }
 		public string Answer { get; set; }
 		public int PortalId { get; set; }
@@ -37,15 +37,15 @@ namespace TemplateFw.Dtos
 		public DateTime CreationDate { get; set; }
 		public string LastModifiedBy { get; set; }
 		public DateTime? LastModificationDate { get; set; }
-    }
+	}
 
-    public class FaqFilter
-    {
-        public string Question { get; set; }
+	public class FaqFilter
+	{
+		public string Question { get; set; }
 		public int? PortalId { get; set; }
 		public int? ServiceId { get; set; }
 		public int LanguageId { get; set; } = 1;
 		public int PageNumber { get; set; } = 1;
 		public int PageSize { get; set; } = 20;
-    }
+	}
 }
